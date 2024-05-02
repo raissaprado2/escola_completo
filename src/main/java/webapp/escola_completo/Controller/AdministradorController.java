c5dd7309ee20384fb72ca48593c4d9cdce65d526
raissaprado2 <ra.pradoviturino@gmail.com>
2024-05-02 16:56:03 -0300

    02/05

--- src/main/java/webapp/escola_completo/Controller/AdministradorController.java
+++ src/main/java/webapp/escola_completo/Controller/AdministradorController.java
@@ -11,7 +11,6 @@ import webapp.escola_completo.Repository.AdministradorRepository;
 import webapp.escola_completo.Repository.VerificaCadastroAdmRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
--- src/main/java/webapp/escola_completo/Repository/AdministradorRepository.java
+++ src/main/java/webapp/escola_completo/Repository/AdministradorRepository.java
@@ -2,12 +2,13 @@ package webapp.escola_completo.Repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import webapp.escola_completo.Controller.Aluno;
 import webapp.escola_completo.Model.Administrador;
 
-
-public interface AdministradorRepository extends CrudRepository<Administrador, String>{
+public interface AdministradorRepository extends CrudRepository<Administrador, String> {
     Administrador findByCpf(String cpf);
     Administrador findBySenha(String senha);
 
-    public void save(Object adm);
+    @Override
+    <S extends Administrador> S save(Aluno aluno);
 }
